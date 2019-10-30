0ca3fd1f80fc2028927d5fd613722bafc3dd9f18
BeginEndDeveloper <52324286+BeginEndDeveloper@users.noreply.github.com>
2019-10-30 21:31:56 +0300

    Add files via upload

---
+++ 03 Circle Bool/uMain.pas
@@ -0,0 +1,27 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Ani,
+  FMX.Objects;
+
+type
+  TForm1 = class(TForm)
+    Circle1: TCircle;
+    FloatAnimation1: TFloatAnimation;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
