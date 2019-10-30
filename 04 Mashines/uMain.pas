51467038f29ab92e7791ff6f19fc5095a41579d6
BeginEndDeveloper <52324286+BeginEndDeveloper@users.noreply.github.com>
2019-10-30 21:32:37 +0300

    Add files via upload

---
+++ 04 Mashines/uMain.pas
@@ -0,0 +1,54 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Ani, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+
+type
+  TForm2 = class(TForm)
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Rectangle3: TRectangle;
+    Rectangle4: TRectangle;
+    Rectangle5: TRectangle;
+    Rectangle6: TRectangle;
+    Rectangle7: TRectangle;
+    Rectangle8: TRectangle;
+    Rectangle9: TRectangle;
+    Rectangle10: TRectangle;
+    Rectangle11: TRectangle;
+    Rectangle12: TRectangle;
+    Rectangle13: TRectangle;
+    Rectangle14: TRectangle;
+    Rectangle15: TRectangle;
+    Rectangle16: TRectangle;
+    recBlue: TRectangle;
+    recYellow: TRectangle;
+    FloatAnimation1: TFloatAnimation;
+    FloatAnimation2: TFloatAnimation;
+    Button1: TButton;
+    Layout1: TLayout;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  Layout1.AnimateFloat('Opacity', 0, 1);
+end;
+
+end.
