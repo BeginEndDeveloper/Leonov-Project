4c3dc35709766c1e6c45f130cba22fd76af7b0d4
BeginEndDeveloper <52324286+BeginEndDeveloper@users.noreply.github.com>
2020-01-03 20:41:17 +0300

    Update 10 project

--- 10 Diary of observation/uMian.pas
+++ 10 Diary of observation/uMian.pas
@@ -8,7 +8,13 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Actions,
   FMX.ActnList, FMX.TabControl, FMX.StdCtrls, FMX.Controls.Presentation,
   FMX.ListBox, FMX.Layouts, FMX.Edit, FMX.EditBox, FMX.SpinBox, FMX.ScrollBox,
-  FMX.Memo, FMX.DateTimeCtrls, FMX.StdActns, FMX.MediaLibrary.Actions;
+  FMX.Memo, FMX.DateTimeCtrls, FMX.StdActns, FMX.MediaLibrary.Actions,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.FMXUI.Wait, Data.DB, FireDAC.Comp.Client,
+  FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
+  FireDAC.Comp.DataSet;
 
 type
   TMain = class(TForm)
@@ -18,14 +24,14 @@ type
     TabItem3: TTabItem;
     ToolBar1: TToolBar;
     SpeedButton1: TSpeedButton;
-    ActionList1: TActionList;
+    ActionList: TActionList;
     ChangeTabAction1: TChangeTabAction;
     ToolBar2: TToolBar;
     SpeedButton2: TSpeedButton;
     SpeedButton3: TSpeedButton;
     SpeedButton4: TSpeedButton;
     ChangeTabAction2: TChangeTabAction;
-    ListBox1: TListBox;
+    lbTask: TListBox;
     ChangeTabAction3: TChangeTabAction;
     ToolBar3: TToolBar;
     SpeedButton5: TSpeedButton;
@@ -66,20 +72,22 @@ type
     SpeedButton9: TSpeedButton;
     Label1: TLabel;
     ShowShareSheetAction1: TShowShareSheetAction;
+    fdcBase: TFDConnection;
+    fdtTask: TFDTable;
+    fdQuery: TFDQuery;
     procedure FormCreate(Sender: TObject);
     procedure SpeedButton4Click(Sender: TObject);
     procedure SpeedButton8Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
-    procedure ListBox1Click(Sender: TObject);
+    procedure lbTaskClick(Sender: TObject);
     procedure ShowShareSheetAction1BeforeExecute(Sender: TObject);
     procedure SpeedButton9Click(Sender: TObject);
+    procedure fdcBaseAfterConnect(Sender: TObject);
+    procedure fdcBaseBeforeConnect(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
   private
     { Private declarations }
-    arDate: array [1 .. 31] of TDate; // хранение даты
-    arTime: array [1 .. 31] of TTime; // хранение времени
-    arLength: array [1 .. 31] of single; // хранение длины
-    arWater: array [1 .. 31] of boolean; // хранение полива
-    arComment: array [1 .. 31] of string; // хранение
+    procedure AddListBoxItem(aDate, aTime: string; aLenght: single);
   public
     { Public declarations }
   end;
@@ -91,135 +99,85 @@ implementation
 
 {$R *.fmx}
 
-procedure TMain.FormCreate(Sender: TObject);
+procedure TMain.AddListBoxItem(aDate, aTime: string; aLenght: single);
 var
-  i: integer;
+  NewListBoxItem: TListBoxItem;
 begin
-  TabControl1.ActiveTab := TabItem1;
-  //
-  for i := 1 to 31 do
-  begin
-    arLength[i] := 0.0;
-    arDate[i] := 0;
-    arTime[i] := 0;
-    arWater[i] := false;
-    arComment[i] := '';
-  end;
+  NewListBoxItem := TListBoxItem.Create(lbTask);
+  NewListBoxItem.Parent := lbTask;
+  NewListBoxItem.StyleLookup := 'listboxitembottomdetail';
+  NewListBoxItem.Height := 44;
+  NewListBoxItem.ItemData.Accessory := TListBoxItemData.TAccessory.aMore;
+  NewListBoxItem.ItemData.Text := aDate + ' ' + aTime;
+  NewListBoxItem.ItemData.Detail := 'Длина ростка ' + FloatToStr(aLenght) + ' мм';
+end;
 
+procedure TMain.fdcBaseAfterConnect(Sender: TObject);
+begin
+  fdcBase.ExecSQL('CREATE TABLE IF NOT EXISTS Task (' + //
+    'ID INTEGER NOT NULL, ' + // ID
+    'Length REAL NOT NULL, ' + // длины
+    'Date TEXT NOT NULL, ' + // даты
+    'Time TEXT NOT NULL, ' + // времени
+    'Water INTEGER NOT NULL, ' + // полива
+    'Comment TEXT ' + // коммент
+    ')');
+end;
+
+procedure TMain.fdcBaseBeforeConnect(Sender: TObject);
+begin
+{$IF DEFINED(MSWINDOWS)}
+  // IncludeTrailingBackslash
+  fdcBase.Params.Values['Database'] := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + 'tasks.sdb';
+{$ELSE}
+  fdcBase.Params.Values['Database'] := TPath.GetDocumentsPath + PathDelim + 'tasks.sdb';
+{$ENDIF}
+end;
+
+procedure TMain.FormCreate(Sender: TObject);
+begin
+  try
+    fdcBase.Connected := True;
+    fdtTask.TableName := 'Task';
+    fdtTask.Active := True;
+  except
+    on e: Exception do
+    begin
+      ShowMessage(e.Message);
+    end;
+  end;
+  TabControl1.ActiveTab := TabItem1;
 end;
 
 procedure TMain.FormShow(Sender: TObject);
-var
-  FileName: string; // Полное имя файла
-  MyFile: TextFile; // файл перемеменная
-  BigString: string; // Строка для чтения файла
-  sDate, sTime, sLength, sWater, sComment: string;
-  i, n, j: integer;
-  NewListBoxItem: TListBoxItem;
 begin
-  // Получить полное имя файла с путём
-  exit;
-  try
-    FileName := TPath.GetDocumentsPath + PathDelim + 'diary.txt';
-  except
-    on E: exception do
-      ShowMessage(E.Message);
-  end;
-  // Связать файловую переменную с именем файла
-  AssignFile(MyFile, FileName);
-  // Открыть файл на чтение
-  Reset(MyFile);
-  // Пока не закончится файл
-  n := 0;
-  while not Eof(MyFile) do
+  fdtTask.First;
+  while not fdtTask.Eof do
   begin
-    // считываем строки
-    Readln(MyFile, BigString);
-    //
-    sDate := '';
-    sTime := '';
-    sLength := '';
-    sWater := '';
-    sComment := '';
-
-    i := Low(BigString);
-    while BigString[i] <> ' ' do
-    begin
-      sDate := sDate + BigString[i];
-      i := i + 1;
-    end;
-    i := i + 1;
-    while BigString[i] <> ' ' do
-    begin
-      sTime := sTime + BigString[i];
-      i := i + 1;
-    end;
-    i := i + 1;
-    while BigString[i] <> ' ' do
-    begin
-      sLength := sLength + BigString[i];
-      i := i + 1;
-    end;
-    i := i + 1;
-    while BigString[i] <> ' ' do
-    begin
-      sWater := sWater + BigString[i];
-      i := i + 1;
-    end;
-    i := i + 1;
-    while i < High(BigString) do
-    begin
-      if BigString[i] <> '"' then
-        sComment := sComment + BigString[i];
-      i := i + 1;
-    end;
-    //
-
-    arDate[n] := StrToDate(sDate);
-    arTime[n] := StrToTime(sTime);
-    arLength[n] := StrToFloat(sLength);
-    if sWater = 'да' then
-      arWater[n] := true
-    else
-      arWater[n] := false;
-    arComment[n] := sComment;
-    //
-    n := n + 1;
-  end;
-  // Закрываем файл
-  CloseFile(MyFile);
-  for j := 1 to n do
-  begin
-    NewListBoxItem := TListBoxItem.Create(ListBox1);
-    NewListBoxItem.Parent := ListBox1;
-    NewListBoxItem.StyleLookup := 'listboxitembottomdetail';
-    NewListBoxItem.Height := 44;
-    NewListBoxItem.ItemData.Accessory := TListBoxItemData.TAccessory.aMore;
-    NewListBoxItem.ItemData.Text := DateToStr(arDate[j]) + ' ' +
-      TimeToStr(arTime[j]);
-    NewListBoxItem.ItemData.Detail := 'Длина ростка ' +
-      FloatToStr(arLength[j]) + ' мм';
+    AddListBoxItem(fdtTask.FieldByName('Date').AsString, fdtTask.FieldByName('Time').AsString, fdtTask.FieldByName('Length').AsFloat);
+    fdtTask.Next;
   end;
 end;
 
-procedure TMain.ListBox1Click(Sender: TObject);
-var
-  ListBoxItemIndex: integer;
+procedure TMain.lbTaskClick(Sender: TObject);
 begin
   // Если элемент не выбран
-  if ListBox1.ItemIndex < 0 then
+  if lbTask.ItemIndex < 0 then
     exit;
   // Индекс элемента
-  ListBoxItemIndex := ListBox1.ItemIndex + 1;
-  laDateTimeValue.Text := DateToStr(arDate[ListBoxItemIndex]) + ' ' +
-    TimeToStr(arTime[ListBoxItemIndex]);
-  laLenghtValue.Text := 'Длина ростка ' +
-    FloatToStr(arLength[ListBoxItemIndex]) + ' мм';
-  if arWater[ListBoxItemIndex] then
+  fdQuery.Close;
+  fdQuery.SQL.Clear;
+  fdQuery.SQL.Add('select * from task where id = :id');
+  fdQuery.ParamByName('id').AsInteger := lbTask.ItemIndex + 1;
+  fdQuery.Open;
+  laDateTimeValue.Text := fdQuery.FieldByName('Date').AsString + ' ' + fdQuery.FieldByName('Time').AsString;
+  laLenghtValue.Text := 'Длина ростка ' + fdQuery.FieldByName('Length').AsString + ' мм';
+  if fdQuery.FieldByName('Water').AsInteger = 1 then
     laWaterValue.Text := 'Да'
   else
     laWaterValue.Text := 'Нет';
-  laCommentValue.Text := arComment[ListBoxItemIndex];
+  laCommentValue.Text := fdQuery.FieldByName('Comment').AsString;
+  fdQuery.Close;
   // Навигация на вторую страницу
   ChangeTabAction3.ExecuteTarget(TabControl1);
 end;
@@ -229,51 +187,44 @@ begin
   ShowShareSheetAction1.TextMessage := 'Hello from mobile!';
 end;
 
-procedure TMain.SpeedButton4Click(Sender: TObject);
-var
-  NewIndex: integer;
-  NewListBoxItem: TListBoxItem;
-
-  MyFile: TextFile;
-  FileName: string;
-  MyString: string;
+procedure TMain.SpeedButton1Click(Sender: TObject);
 begin
-  // ShowMessage('Запись сохранена');
+  //
+  deDateNew.Text := DateToStr(Date);
+  teTimeNew.Text := TimeToStr(Now);
+  sbLengthNew.Value := 0;
+  swWaterNew.IsChecked := False;
+  meCommentNew.Lines.Clear;
+  ChangeTabAction1.ExecuteTarget(TabControl1);
+end;
+
+procedure TMain.SpeedButton4Click(Sender: TObject);
+begin
   // Часть 1. добавление данных в массив
-  NewIndex := ListBox1.Count + 1;
-  arDate[NewIndex] := deDateNew.Date;
-  arTime[NewIndex] := teTimeNew.Time;
-  arLength[NewIndex] := sbLengthNew.Value;
-  arWater[NewIndex] := swWaterNew.IsChecked;
-  arComment[NewIndex] := meCommentNew.Text;
+
+  (* fdQuery.Close;
+    fdQuery.SQL.Clear;
+    fdQuery.SQL.Add('Insert into Task (ID, Length, Date, Time, Water, Comment) ');
+    fdQuery.SQL.Add('values (:ID, :L, :D, :T, :W, :C)');
+    fdQuery.ParamByName('ID').AsInteger := lbTask.Count + 1;
+    fdQuery.ParamByName('L').AsFloat := sbLengthNew.Value;
+    fdQuery.ParamByName('D').AsString := DateToStr(deDateNew.Date);
+    fdQuery.ParamByName('T').AsString := TimeToStr(teTimeNew.Time);
+    fdQuery.ParamByName('W').AsInteger := swWaterNew.IsChecked.ToInteger;
+    fdQuery.ParamByName('C').AsString := meCommentNew.Text;
+    fdQuery.ExecSQL; *)
+
+  // new
+  try
+    fdQuery.ExecSQL('Insert into Task (ID, Length, Date, Time, Water, Comment) ' + 'values (:ID, :L, :D, :T, :W, :C)',
+      [lbTask.Count + 1, sbLengthNew.Value, DateToStr(deDateNew.Date), TimeToStr(teTimeNew.Time), swWaterNew.IsChecked.ToInteger, meCommentNew.Text]);
+  except
+    on e: Exception do
+      ShowMessage(e.Message);
+  end;
+
   // Часть 2, добавление элемента в список
-  NewListBoxItem := TListBoxItem.Create(ListBox1);
-  NewListBoxItem.Parent := ListBox1;
-  NewListBoxItem.StyleLookup := 'listboxitembottomdetail';
-  NewListBoxItem.Height := 44;
-  NewListBoxItem.ItemData.Accessory := TListBoxItemData.TAccessory.aMore;
-  NewListBoxItem.ItemData.Text := DateToStr(arDate[NewIndex]) + ' ' +
-    TimeToStr(arTime[NewIndex]);
-  NewListBoxItem.ItemData.Detail := 'Длина ростка ' +
-    FloatToStr(arLength[NewIndex]) + ' мм';
-
-  FileName := TPath.GetDocumentsPath + PathDelim + 'diary.txt';
-  if not FileExists(FileName) then
-    AssignFile(MyFile, FileName);
-
-  if not FileExists(FileName) then
-    Rewrite(MyFile)
-  else
-    Append(MyFile);
-  MyString := DateToStr(arDate[NewIndex]) + ' ' + TimeToStr(arTime[NewIndex]) +
-    ' ' + FloatToStr(arLength[NewIndex]);
-  if arWater[NewIndex] = true then
-    MyString := MyString + ' ' + 'да' + ' '
-  else
-    MyString := MyString + ' ' + 'нет' + ' ';
-  MyString := MyString + '"' + arComment[NewIndex] + '"';
-  Writeln(MyFile, MyString);
-  CloseFile(MyFile);
+  AddListBoxItem(DateToStr(deDateNew.Date), TimeToStr(teTimeNew.Time), sbLengthNew.Value);
   // Переходим на первую страничку
   ChangeTabAction2.ExecuteTarget(TabControl1);
 end;
